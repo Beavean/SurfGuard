8b0dd94dae82f7a1f644bfc8d22dcb557f345e2a
Anton Petrov <beavean@gmail.com>
2023-02-27 14:09:59 +0200

    feat: Add automatic window color changes and UIColor extension for default colors

--- WebsiteFilter/AppDelegate.swift
+++ WebsiteFilter/AppDelegate.swift
@@ -11,11 +11,21 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_: UIApplication,
+                     didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
-        window?.rootViewController = MainViewController()
+        let navigationController = UINavigationController(rootViewController: WebPageViewController())
+        if #available(iOS 13, *) {
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithTransparentBackground()
+            appearance.backgroundEffect = UIBlurEffect(style: .extraLight)
+            navigationController.navigationBar.standardAppearance = appearance
+            navigationController.navigationBar.scrollEdgeAppearance = appearance
+        }
+        window?.rootViewController = navigationController
+        window?.tintColor = .tintColor
+        window?.backgroundColor = .backgroundColor
         return true
     }
 }
